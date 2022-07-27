0ee80b13b2b7f789a3a659a372be4526c2b4189e
Scott Kaplan <scott@kaplans.com>
2022-07-27 02:06:43 +0000

    Clean up indentation.

--- script.js
+++ script.js
@@ -6,21 +6,21 @@ toppings.forEach((topping) => {
   });
 });
 
- const checkoutButton = document.querySelector(".button");
- const answerHolder = document.querySelector("#total");
+const checkoutButton = document.querySelector(".button");
+const answerHolder = document.querySelector("#total");
 
- checkoutButton.addEventListener("click", (e) => {
+checkoutButton.addEventListener("click", (e) => {
   const ingredients = document.querySelectorAll(".order .topping");
   let total = 0;
    ingredients.forEach((ingredient) => {
      total += Number(ingredient.dataset.price);
    });
    answerHolder.innerHTML = `The total cost of your burger is $${total}`;
- });
+});
 
- const navButtons = document.querySelectorAll("#nav .topping");
- const orderDiv = document.querySelector(".order");
- navButtons.forEach((navButton) => {
+const navButtons = document.querySelectorAll("#nav .topping");
+const orderDiv = document.querySelector(".order");
+navButtons.forEach((navButton) => {
    navButton.addEventListener("click", () => {
      orderDiv.innerHTML += `<div class="topping ${navButton.dataset.ingredient}" data-ingredient=${navButton.dataset.ingredient} data-price=${navButton.dataset.price}>${navButton.dataset.ingredient}</div>`;
    });
